d519e136b19a2f66be78a5c9d2da6aad759298a8
Karl Nicholas <Karl.Nicholas@outlook.com>
2016-02-11 22:32:35 -0500

    rename packages. restore primefaces frontend.

---
+++ src/main/resources/archetype-resources/src/main/java/service/package-info.java
@@ -0,0 +1,9 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Beans for accessing persistence layer.
+ * @author Karl Nicholas
+ *
+ */
+package service;
